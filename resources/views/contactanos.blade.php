23e3cf12b73912f512d10550a24b0bb7471f8a0b
fernandapao <fernandapao_@hotmail.com>
2024-10-02 12:30:31 -0300

    contacto

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -2,7 +2,50 @@
 
     <x-slot:title>Contactanos</x-slot:title>
 
-    <h1 class="mb-3">Contactanos!</h1>
-    <div>CONTACTANOS.BLADE</div>
+    <div class="container text-center mt-5">
+        <a class="navbar-brand" href="<?= url('inicio') ?>">
+            <img src="img/aero-asist-01.png" alt="logo de AeroAsist" class="logo-nav img-fluid" style="max-width: 400px;">
+        </a>
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="text-center ">¡Ponte en contacto con nosotros!</h1>
+        <p class="text-center fs-2 text">Si tienes alguna pregunta o necesitas más información sobre nuestros servicios, no dudes en escribirnos. Estaremos encantados de atenderte.</p>
+
+        <div class="card shadow p-4 mb-5 bg-white rounded">
+        <form action="#" method="POST" class="mt-4">
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="name" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+
+                <div class="col-md-6 mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="phone" class="form-label">Teléfono</label>
+                    <input type="text" class="form-control" id="phone" name="phone" required>
+                </div>
+
+                <div class="col-md-6 mb-3">
+                    <label for="message" class="form-label">Mensaje</label>
+                    <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+                </div>
+            </div>
+
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </form>
+    </div>
+</div>
+
 
 </x-layout>
+
+
